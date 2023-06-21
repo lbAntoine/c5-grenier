6c98158058d724c8103df7cf6d928662ba1cbc11
lbAntoine <antoine.lebras@gmail.com>
2023-06-21 09:18:31 +0200

    Found solution

--- docker/prod/deploy.sh
+++ docker/prod/deploy.sh
@@ -4,7 +4,7 @@
 # cd /home/lbantoine/code/c5-grenier/docker/prod && docker-compose down
 
 # Get the latest code
-cd /home/lbantoine/code/c5-grenier && git pull && docker network prune -f && docker image prune -f && docker container prune -f && docker-compose -f /home/lbantoine/code/c5-grenier/docker/prod/docker-compose.server.yml up -d && echo "$(date '+%H:%M:%S   %d/%m/%y') -> Deployment done for commit: $(git log -1 --pretty=%B)" >> /home/lbantoine/code/c5-grenier.log
+cd /home/lbantoine/code/c5-grenier && git pull && docker network prune -f && docker image prune -f && docker container prune -f && docker-compose -f /home/lbantoine/code/c5-grenier/docker-compose.server.yml up -d && echo "$(date '+%H:%M:%S   %d/%m/%y') -> Deployment done for commit: $(git log -1 --pretty=%B)" >> /home/lbantoine/code/c5-grenier.log
 
 # Clean docker network
 # docker network prune -f
